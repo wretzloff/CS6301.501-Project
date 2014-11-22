7071db2dd93878d571131749d211b13a8129f240
Will Retzloff <wretzloff@gmail.com>
2014-11-21 21:21:49 -0600

    Incremental cleanup

--- web2py/applications/welcome/controllers/default.py
+++ web2py/applications/welcome/controllers/default.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - api is an example of Hypermedia API support and access control
-#########################################################################
-
 @auth.requires_login()
 def index():
     response.title = "Inbox for " + auth.user.email
@@ -26,18 +15,6 @@ def compose():
     else:
         response.flash = 'please fill the form'
     return dict(formToDisplay=form)
-
-def teamA():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
-
 
 def user():
     """
